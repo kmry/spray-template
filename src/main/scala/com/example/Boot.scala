d93e035de0a0c0f82236984409281fddf29cd767
Mathias <mathias@spray.cc>
2012-04-05 23:34:23 +0200

    Upgrade to spray 1.0-M1

--- src/main/scala/com/example/Boot.scala
+++ src/main/scala/com/example/Boot.scala
@@ -1,31 +1,50 @@
 package com.example
 
-import org.slf4j.LoggerFactory
-import akka.config.Supervision._
-import akka.actor.{Supervisor, Actor}
+import akka.actor.{Props, ActorSystem}
 import cc.spray.{SprayCanRootService, HttpService}
-import cc.spray.can.HttpServer
+import cc.spray.io.IoWorker
+import cc.spray.can.server.HttpServer
+import cc.spray.io.pipelines.MessageHandlerDispatch
+
 
 object Boot extends App {
+  // we need an ActorSystem to host our application in
+  val system = ActorSystem("demo")
 
-  LoggerFactory.getLogger(getClass) // initialize SLF4J early
-
+  // create the service instance, supplying all required dependencies
   val mainModule = new HelloService {
+    implicit def actorSystem = system
     // bake your module cake here
   }
 
-  val httpService    = Actor.actorOf(new HttpService(mainModule.helloService))
-  val rootService    = Actor.actorOf(new SprayCanRootService(httpService))
-  val sprayCanServer = Actor.actorOf(new HttpServer())
-
-  Supervisor(
-    SupervisorConfig(
-      OneForOneStrategy(List(classOf[Exception]), 3, 100),
-      List(
-        Supervise(httpService, Permanent),
-        Supervise(rootService, Permanent),
-        Supervise(sprayCanServer, Permanent)
-      )
-    )
+  // create and start the HttpService actor running our service as well as the root actor
+  val httpService = system.actorOf(
+    props = Props(new HttpService(mainModule.helloService)),
+    name = "http-service"
   )
+  val rootService = system.actorOf(
+    props = Props(new SprayCanRootService(httpService)),
+    name = "root-service"
+  )
+
+  // every spray-can HttpServer (and HttpClient) needs an IoWorker for low-level network IO
+  // (but several servers and/or clients can share one)
+  val ioWorker = new IoWorker(system).start()
+
+  // create and start the spray-can HttpServer, telling it that we want requests to be
+  // handled by the root service actor
+  val sprayCanServer = system.actorOf(
+    Props(new HttpServer(ioWorker, MessageHandlerDispatch.SingletonHandler(rootService))),
+    name = "http-server"
+  )
+
+  // a running HttpServer can be bound, unbound and rebound
+  // initially to need to tell it where to bind to
+  sprayCanServer ! HttpServer.Bind("localhost", 8080)
+
+  // finally we drop the main thread but hook the shutdown of
+  // our IoWorker into the shutdown of the applications ActorSystem
+  system.registerOnTermination {
+    ioWorker.stop()
+  }
 }
